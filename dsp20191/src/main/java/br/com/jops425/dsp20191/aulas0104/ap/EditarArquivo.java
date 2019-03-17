890ad7cec8c9761424beae70ccd5b8b02d7f75c9
João Pedro Vieira <jp-a.v@hotmail.com>
2019-03-16 23:27:38 -0300

    Adicionada classe EditarArquivo

---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0104/ap/EditarArquivo.java
@@ -0,0 +1,27 @@
+package br.com.jops425.dsp20191.aulas0104.ap;
+
+import java.io.IOException;
+
+public class EditarArquivo {
+
+    private final static int numLinhas = 5;
+
+    public static void main(String[] args) throws IOException {
+        String caminho = "src/main/resources/aula0104.txt";
+        String conteudo = gerarLinhas(numLinhas);
+        Arquivo.gravar(conteudo, caminho);
+        Arquivo.ler(caminho);
+    }
+
+    public static String gerarLinhas(int num) {
+        String linhas = "";
+        for (int i = 0; i < num; i++) {
+            if (i == 0) {
+                linhas += "\n";
+            }
+            linhas += "Esta é a linha acrescida - número " + (i + 1) + "\n";
+        }
+        return linhas;
+    }
+
+}
