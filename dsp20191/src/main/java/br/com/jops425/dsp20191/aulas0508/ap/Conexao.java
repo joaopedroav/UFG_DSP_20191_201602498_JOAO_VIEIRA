efc3f0185758521e53b49ce0648a4db8d95e9b25
João Pedro Vieira <jp-a.v@hotmail.com>
2019-03-20 14:53:21 -0300

    Adicionado pacote aulas0508

---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0508/ap/Conexao.java
@@ -0,0 +1,14 @@
+package br.com.jops425.dsp20191.aulas0508.ap;
+
+public class Conexao {
+
+    private String sgbd;
+
+    private String tipoConexao;
+
+    private String bancoDados;
+
+    private String urlBd;
+
+    private String SistemaOperacional;
+}
---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0508/ap/Usuario.java
@@ -0,0 +1,10 @@
+package br.com.jops425.dsp20191.aulas0508.ap;
+
+public class Usuario {
+
+    private String nome;
+
+    private String login;
+
+    private String senha;
+}
