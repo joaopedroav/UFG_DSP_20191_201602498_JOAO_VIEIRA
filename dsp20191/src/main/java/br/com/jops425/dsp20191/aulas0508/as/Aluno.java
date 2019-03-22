fac5ad4ed134ab0ace37b62174789b2672c01502
João Pedro Vieira <jp-a.v@hotmail.com>
2019-03-22 09:09:11 -0300

    Add pacote as

---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0508/as/Aluno.java
@@ -0,0 +1,45 @@
+package br.com.jops425.dsp20191.aulas0508.as;
+
+public class Aluno {
+
+    private String nome;
+
+    private long matricula;
+
+    private double nota1;
+
+    private double nota2;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nom) {
+        this.nome = nom;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matric) {
+        this.matricula = matric;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double not1) {
+        this.nota1 = not1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+}
