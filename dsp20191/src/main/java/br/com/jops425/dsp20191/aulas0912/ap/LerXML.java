fbffcb8f36422827e6d74bee735c6eb33d502878
João Pedro Vieira <jp-a.v@hotmail.com>
2019-03-30 16:31:37 -0300

    Atualizado exercício 9

--- dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0912/ap/LerXML.java
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas0912/ap/LerXML.java
@@ -1,13 +1,19 @@
 package br.com.jops425.dsp20191.aulas0912.ap;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.Node;
+import org.dom4j.DocumentHelper;
 import org.dom4j.io.SAXReader;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
+
 
 /**
  * Created by aluno on 28/03/19.
@@ -19,6 +25,7 @@ public class LerXML {
         final String tags = "/class/student";
         List<Node> nodes = ler(caminho, tags);
         imprimir(nodes);
+        gerar();
     }
 
     public static List<Node> ler(final String caminho, final String tags) {
@@ -52,4 +59,37 @@ public class LerXML {
             }
         }
     }
+
+    public static void gerar() {
+        try {
+            Document document = DocumentHelper.createDocument();
+            Element root = document.addElement( "class" );
+            Element studentElement = root.addElement("students");
+
+            studentElement.addElement("matricula").addText("201602498");
+            studentElement.addElement("firstname").addText("JOÃO PEDRO");
+            studentElement.addElement("lastname").addText("VIEIRA");
+            studentElement.addElement("nickname").addText("joao.vieira");
+
+            Element frequencyElement = studentElement.addElement("frequencia");
+
+            frequencyElement.addElement("aulas")
+                    .addAttribute("numero", "0104")
+                    .addText("P");
+
+            frequencyElement.addElement("aulas")
+                    .addAttribute("numero", "0508")
+                    .addText("P");
+
+            OutputFormat format = OutputFormat.createPrettyPrint();
+            XMLWriter writer;
+            writer = new XMLWriter( System.out, format );
+            writer.write( document );
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
