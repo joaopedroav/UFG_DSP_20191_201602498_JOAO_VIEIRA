110e87b57f018c34e1140cddad369a1bd83dbca2
João Pedro Vieira <jp-a.v@hotmail.com>
2019-04-03 20:54:20 -0300

    Atualizações

---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas1316/ap/exceptions/InvalidDataBaseTypeException.java
@@ -0,0 +1,7 @@
+package br.com.jops425.dsp20191.aulas1316.ap.exceptions;
+
+public class InvalidDataBaseTypeException extends Exception{
+    public InvalidDataBaseTypeException(String message){
+        super(message);
+    }
+}
