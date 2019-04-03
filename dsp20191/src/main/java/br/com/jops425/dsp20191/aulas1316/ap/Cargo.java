1c819cf750af39e9dab54ac6593b8f19215a99a3
João Pedro Vieira <jp-a.v@hotmail.com>
2019-04-03 19:14:43 -0300

    Adicionado pacote aulas1316

---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas1316/ap/Cargo.java
@@ -0,0 +1,27 @@
+package br.com.jops425.dsp20191.aulas1316.ap;
+
+/**
+ * Created by aluno on 03/04/19.
+ */
+public class Cargo {
+
+    private String id;
+
+    private String nome;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas1316/ap/Funcionario.java
@@ -0,0 +1,37 @@
+package br.com.jops425.dsp20191.aulas1316.ap;
+
+/**
+ * Created by aluno on 03/04/19.
+ */
+public class Funcionario {
+
+    private long id;
+
+    private String nome;
+
+    private long matricula;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+}
