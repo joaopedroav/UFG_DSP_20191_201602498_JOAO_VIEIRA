c6ac8cfb6cf9016a9105c7c57db044e6b5d6d476
João Pedro Vieira <jp-a.v@hotmail.com>
2019-04-15 17:40:00 -0300

    Remoção de printf

--- dsp20191/src/main/java/br/com/jops425/dsp20191/aulas1720/ap/persistence/dml/delete/Delete.java
+++ dsp20191/src/main/java/br/com/jops425/dsp20191/aulas1720/ap/persistence/dml/delete/Delete.java
@@ -32,7 +32,7 @@ public class Delete extends PersistenciaJdbc {
                 System.out.println("Cargo excluído corretamente");
                 ps.close();
             } else {
-                System.out.printf("Cargo com id %ld não encontrado", id);
+                System.out.println("Cargo com id " + id + " não encontrado");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -55,7 +55,7 @@ public class Delete extends PersistenciaJdbc {
                 System.out.println("Departamento excluído corretamente");
                 ps.close();
             } else {
-                System.out.printf("Departamento com id %ld não encontrado", id);
+                System.out.println("Departamento com id " + id + " não encontrado");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -78,7 +78,7 @@ public class Delete extends PersistenciaJdbc {
                 System.out.println("Funcionario excluído corretamente");
                 ps.close();
             } else {
-                System.out.printf("Funcionario com id %ld não encontrado", id);
+                System.out.println("Funcionario com id " + id + " não encontrado");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -101,7 +101,7 @@ public class Delete extends PersistenciaJdbc {
                 System.out.println("Lotação excluída corretamente");
                 ps.close();
             } else {
-                System.out.printf("Lotação com id %ld não encontrado", id);
+                System.out.println("Lotação com id " + id +" não encontrado");
             }
         } catch (Exception e) {
             e.printStackTrace();
